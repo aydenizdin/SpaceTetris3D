d96e6f9e9e0a8d70af69dc889c0bb6c70295b067
daydin <daydin@ilkebilisimmerkezi.com.tr>
2020-10-13 03:14:29 +0300

    dsdsa

--- Assets/Scripts/Platform/Platform.cs
+++ Assets/Scripts/Platform/Platform.cs
@@ -3,28 +3,95 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.UIElements;
 
- 
+
 public class Platform : MonoBehaviour
 {
-     [SerializeField] 
-     public PlatformDimensions PlatDims;
-     public LineRenderer LinesRenderer;
 
-     public T3DConfiguration t3dConfiguration;
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-        PlatformDrawer pd = new PlatformDrawer(t3dConfiguration, LinesRenderer);
-        
-        pd.DrawPlatform(new PlatformDimensions()
-        {
-            Width =PlatDims.Width,
-            Depth = PlatDims.Depth,
-            Height = PlatDims.Height
-        });
-    }
+
+   private List<PlatformLineSegment> linestorender; // todo temp bunu sil deneme gl line rendering
+   /// <summary>
+   /// ////////////
+   /// </summary>
+   [SerializeField] public PlatformDimensions PlatDims;
+   
+
+   public T3DConfiguration t3dConfiguration;
 
     
+
+   // Start is called before the first frame update
+   void Start()
+   {
+     // GameObject instanceGrid = Instantiate(GridPref, Vector3.zero, GridPref.transform.rotation);
+
+      PlatformDrawer pd = new PlatformDrawer(t3dConfiguration);
+      
+      ///instanceGrid.GetComponent<Renderer>().sharedMaterial.SetFloat("_LineSize"0.7f); // mesela erişim
+      /// 
+      linestorender=  pd.ConstructPlatformVertices(new PlatformDimensions()
+      {
+         
+         Width = PlatDims.Width,
+         Depth = PlatDims.Depth,
+         Height = PlatDims.Height
+      });
+ 
+      CreateLineMaterial();
+       
+   }
+
+    
+   
+
+   static Material lineMaterial;
+   
+   
+   private void CreateLineMaterial()
+   {
+       
+       
+       // Unity has a built-in shader that is useful for drawing
+         // simple colored things.
+         Shader shader = Shader.Find("Hidden/Internal-Colored");
+         lineMaterial = new Material(shader);
+         lineMaterial.hideFlags = HideFlags.HideAndDontSave;
+         // Turn on alpha blending
+         lineMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+         lineMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+         // Turn backface culling off
+         lineMaterial.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
+         // Turn off depth writes
+         lineMaterial.SetInt("_ZWrite", 0);
+      
+   }
+
+   // Will be called after all regular rendering is done
+   public void OnRenderObject()
+   {
+      
+      // Apply the line material
+      lineMaterial.SetPass(0);
+
+      GL.PushMatrix();
+      // Set transformation matrix for drawing to
+      // match our transform
+      GL.MultMatrix(transform.localToWorldMatrix);
+
+      // Draw lines
+      GL.Begin(GL.LINES);
+      // for (int segmentIndex = 1; segmentIndex < linestorender.Count; ++segmentIndex)
+      foreach (PlatformLineSegment eachSegment in linestorender)
+      {
+         GL.Color(Color.magenta);
+         // One vertex at transform position
+         GL.Vertex3(eachSegment.Start.x,eachSegment.Start.y,eachSegment.Start.z);
+         // Another vertex at edge of circle
+         GL.Vertex3(eachSegment.End.x,eachSegment.End.y,eachSegment.End.z);
+      }
+      
+      GL.End();
+      GL.PopMatrix();
+   }
 }
--- Assets/Scripts/Platform/PlatformDrawer.cs
+++ Assets/Scripts/Platform/PlatformDrawer.cs
@@ -1,22 +1,38 @@
-﻿using System;
+﻿#region
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+
+#endregion
+
+public struct PlatformVertex
+{
+   public float x;
+   public float y;
+   public float z;
+}
+
+public struct PlatformLineSegment
+{
+   public PlatformVertex Start;
+   public PlatformVertex End;
+}
 
 public class PlatformDrawer
 {
-   private LineRenderer _lineRenderer;
-   private T3DConfiguration _conf;
-   
-   public PlatformDrawer(T3DConfiguration conf, LineRenderer lRenderer)
+   private readonly T3DConfiguration _conf;
+
+
+   public PlatformDrawer(T3DConfiguration conf)
    {
       _conf = conf;
-      _lineRenderer = lRenderer;
-      _lineRenderer.widthMultiplier = 0.01f;
    }
 
-   public void DrawPlatform(PlatformDimensions dims)
+  
+
+
+   public List<PlatformLineSegment> ConstructPlatformVertices(PlatformDimensions dims)
    {
       //Test
       //     Vector3[] positions = new Vector3[3] { new Vector3(0, 0, 0), new Vector3(-1, 1, 0), new Vector3(1, 1, 0) };
@@ -24,101 +40,180 @@ public class PlatformDrawer
       //_lineRenderer.loop = true;
       //   _lineRenderer.SetPositions(positions);
 
-      List<Vector3> totalPositions = new List<Vector3>(0);
+      List<PlatformLineSegment> totalSegments = new List<PlatformLineSegment>(0);
 
-      Vector3[] startFramePositions = ConstructStartFramePositions(dims);
-      Vector3[] leftSideFramePositions = ConstructSideFramesPositions_Left(dims);
-      Vector3[] rightSideFramePositionsd = ConstructSideFramesPositions_Right(dims);
-      List<Vector3> bottomLines = ConstructBottomLines(dims);
+      PlatformLineSegment[] startFrameSegments = ConstructStartFramePositions(dims);
       
-      totalPositions.AddRange(startFramePositions);
-      totalPositions.AddRange(leftSideFramePositions);
-      totalPositions.AddRange(rightSideFramePositionsd);
-      totalPositions.AddRange(bottomLines);
-      
-      _lineRenderer.positionCount = totalPositions.Count;
-      _lineRenderer.SetPositions(totalPositions.ToArray());
-      
-      
+      // Vector3[] leftSideFramePositions = ConstructSideFramesPositions_Left(dims);
+      // Vector3[] rightSideFramePositionsd = ConstructSideFramesPositions_Right(dims);
+      // List<Vector3> bottomHorLines = ConstructBottomHorizLines(dims);
+      // List<Vector3> bottomVertLines = ConstructBottomVertLines(dims);
+
+
+      totalSegments.AddRange(startFrameSegments);
+
+      return totalSegments;
+      // totalPositions.AddRange(leftSideFramePositions);
+      // totalPositions.AddRange(rightSideFramePositionsd);
+      // totalPositions.AddRange(bottomHorLines);
+      // totalPositions.AddRange(bottomVertLines);
    }
 
-   private  Vector3[]ConstructStartFramePositions(PlatformDimensions dims)
+   private PlatformLineSegment[] ConstructStartFramePositions(PlatformDimensions dims)
    {
+      float unit = _conf.OneUnit;
       float yatayMove = _conf.OneUnit * dims.Width / 2.0f;
       float dikeyMove = _conf.OneUnit * dims.Height / 2.0f;
 
-      Vector3[] StartFramePoss = new Vector3[]
+      PlatformLineSegment[] StartFramePoss = new PlatformLineSegment[4];
+
+      PlatformVertex solUstStart = new PlatformVertex()
       {
-         new Vector3(-yatayMove, dikeyMove, 0),
-         new Vector3(yatayMove, dikeyMove, 0),
-         new Vector3(yatayMove, -dikeyMove, 0),
-         new Vector3(-yatayMove, -dikeyMove, 0),
-         //new Vector3(-yatayMove, dikeyMove, 0) // start frame ile çakışıyor, kaldırıldı
+         x = -yatayMove,
+         y = dikeyMove,
+         z = 0
       };
+
+      PlatformVertex sagUst = new PlatformVertex()
+      {
+         x = yatayMove,
+         y = dikeyMove,
+         z = 0
+      };
+
+      PlatformVertex sagAlt = new PlatformVertex()
+      {
+         x = yatayMove,
+         y = -dikeyMove,
+         z = 0
+      };
+
+      PlatformVertex solAlt = new PlatformVertex()
+      {
+         x = -yatayMove,
+         y = -dikeyMove,
+         z = 0
+      };
+
+      PlatformLineSegment ust = new PlatformLineSegment()
+      {
+         Start = solUstStart,
+         End = sagUst
+      };
+
+      PlatformLineSegment sag = new PlatformLineSegment()
+      {
+         Start = sagUst,
+         End = sagAlt
+      };
+
+      PlatformLineSegment alt = new PlatformLineSegment()
+      {
+         Start = sagAlt,
+         End = solAlt
+      };
+
+      PlatformLineSegment sol = new PlatformLineSegment()
+      {
+         Start = solAlt,
+         End = solUstStart
+      };
+
+      StartFramePoss[0] = ust;
+      StartFramePoss[1] = sag;
+      StartFramePoss[2] = alt;
+      StartFramePoss[3] = sol;
+
 
       return StartFramePoss;
    }
 
-   private  Vector3[] ConstructSideFramesPositions_Left(PlatformDimensions dims)
+
+   private Vector3[] ConstructSideFramesPositions_Left(PlatformDimensions dims)
    {
       float yatayMove = _conf.OneUnit * dims.Width / 2.0f;
       float dikeyMove = _conf.OneUnit * dims.Height / 2.0f;
       float derinlikMove = _conf.OneUnit * dims.Depth;
-      
-      Vector3[] SideFrPoss = new Vector3[]
+
+      Vector3[] SideFrPoss =
       {
          new Vector3(-yatayMove, dikeyMove, 0),
          new Vector3(-yatayMove, dikeyMove, derinlikMove),
          new Vector3(-yatayMove, -dikeyMove, derinlikMove),
-         new Vector3(-yatayMove, -dikeyMove, 0),
-         new Vector3(-yatayMove, dikeyMove, 0)
+         new Vector3(-yatayMove, -dikeyMove, 0)
+         //  new Vector3(-yatayMove, dikeyMove, 0)
       };
 
       return SideFrPoss;
    }
-   
-   private  Vector3[] ConstructSideFramesPositions_Right(PlatformDimensions dims)
+
+   private Vector3[] ConstructSideFramesPositions_Right(PlatformDimensions dims)
    {
       float yatayMove = _conf.OneUnit * dims.Width / 2.0f;
       float dikeyMove = _conf.OneUnit * dims.Height / 2.0f;
       float derinlikMove = _conf.OneUnit * dims.Depth;
-      
-      Vector3[] SideFrPoss = new Vector3[]
+
+      Vector3[] SideFrPoss =
       {
-         new Vector3(yatayMove, dikeyMove, 0),
-         new Vector3(yatayMove, dikeyMove, derinlikMove),
-         new Vector3(yatayMove, -dikeyMove, derinlikMove),
          new Vector3(yatayMove, -dikeyMove, 0),
-       //  new Vector3(yatayMove, dikeyMove, 0)
+         new Vector3(yatayMove, -dikeyMove, derinlikMove),
+         new Vector3(yatayMove, dikeyMove, derinlikMove),
+         new Vector3(yatayMove, dikeyMove, 0)
       };
 
       return SideFrPoss;
    }
-   
-   private    List<Vector3>  ConstructBottomLines(PlatformDimensions dims)
+
+   private List<Vector3> ConstructBottomHorizLines(PlatformDimensions dims)
    {
       float yatayMove = _conf.OneUnit * dims.Width / 2.0f;
       float dikeyMove = _conf.OneUnit * dims.Height / 2.0f;
       float derinlikMove = _conf.OneUnit * dims.Depth;
-      
-      
-         
+
+
       List<Vector3> bottomPos = new List<Vector3>();
+      bottomPos.Add(new Vector3(-yatayMove, dikeyMove, 0));
+      bottomPos.Add(new Vector3(-yatayMove, dikeyMove, derinlikMove));
 
       for (int i = 0; i <= dims.Height; i++)
       {
-         Vector3 soldakiPos = new Vector3(-yatayMove , dikeyMove-(_conf.OneUnit*i), derinlikMove);
-         Vector3 sagdakiPos = new Vector3(yatayMove, dikeyMove-(_conf.OneUnit*i), derinlikMove);
-         
+         Vector3 soldakiPos = new Vector3(-yatayMove, dikeyMove - (_conf.OneUnit * i), derinlikMove);
+         Vector3 sagdakiPos = new Vector3(yatayMove, dikeyMove - (_conf.OneUnit * i), derinlikMove);
+
          bottomPos.Add(soldakiPos);
-    
+
          bottomPos.Add(sagdakiPos);
-         
-      //   bottomPos.Add(soldakiPos);
+
+         bottomPos.Add(soldakiPos);
       }
-            
+
 
       return bottomPos;
    }
-   
+
+
+   private List<Vector3> ConstructBottomVertLines(PlatformDimensions dims)
+   {
+      float yatayMove = _conf.OneUnit * dims.Width / 2.0f;
+      float dikeyMove = _conf.OneUnit * dims.Height / 2.0f;
+      float derinlikMove = _conf.OneUnit * dims.Depth;
+
+
+      List<Vector3> bottomPos = new List<Vector3>();
+
+      for (int i = 1; i < dims.Height; i++)
+      {
+         Vector3 yuk = new Vector3(-yatayMove + (_conf.OneUnit * i), dikeyMove, derinlikMove);
+         Vector3 asag = new Vector3(-yatayMove + (_conf.OneUnit * i), -dikeyMove, derinlikMove);
+
+         bottomPos.Add(yuk);
+
+         bottomPos.Add(asag);
+
+         bottomPos.Add(yuk);
+      }
+
+
+      return bottomPos;
+   }
 }
